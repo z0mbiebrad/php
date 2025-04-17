7598dcc038463e0cd70fe740ebdd0c2f192548a2
z0mbiebrad <bradleyt.evans@gmail.com>
2025-04-17 13:22:42 -0400

    index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo "hello world";
+        ?>
+    </h1>
+
+    <p>
+        <?php
+            echo "hello world";
+        ?>
+    </p>
+</body>
+</html>
